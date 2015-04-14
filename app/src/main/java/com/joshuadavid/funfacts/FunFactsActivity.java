f221e5f8af3b1082c20589809697918574ce2c04
JoshuaDavid <jshuadvd@icloud.com>
2015-04-13 20:21:35 -0700

    Created fact changing section

--- app/src/main/java/com/joshuadavid/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/joshuadavid/funfacts/FunFactsActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class FunFactsActivity extends ActionBarActivity {
@@ -12,6 +15,23 @@ public class FunFactsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        // Declare our View variables and assign the views from the layout file
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // The button was clicked, so update the fact label with a new fact
+                String fact = "Ostriches can run faster than horses.";
+                factLabel.setText(fact);
+
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
     }
 
 
