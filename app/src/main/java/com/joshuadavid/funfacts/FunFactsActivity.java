653d50c137b3859a5a10d07ea775ae7b5c10d0ab
JoshuaDavid <jshuadvd@icloud.com>
2015-04-16 00:54:52 -0700

    Building a Release-Ready APK

--- app/src/main/java/com/joshuadavid/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/joshuadavid/funfacts/FunFactsActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class FunFactsActivity extends ActionBarActivity {
